893c884de5705a253ce8c8da292472e442eaab37
Rui <ruivo.rui@gmail.com>
2020-01-07 15:31:05 +0100

    NEW BUILD 05

---
+++ Config.php
@@ -0,0 +1,18 @@
+<?php
+
+// ROUTER FOR CONFIG PAGE
+class Config
+{
+    // LOCAL DATABASE
+    public $Database_Name = "powermanager";
+    public $Database_User = "root";
+    public $Database_Password = "";
+}
+
+/*
+
+// REMOTE DATABASE
+    public $Database_Name = "powermanager";
+    public $Database_User = "p5root";
+    public $Database_Password = "ycvms74b";
+ */

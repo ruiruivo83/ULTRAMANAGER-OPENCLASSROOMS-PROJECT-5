df8d4e733eee712ba3f90f181457932941590b7d
Rui <ruivo.rui@gmail.com>
2020-01-15 17:23:27 +0100

    Try catch for Offline Database

--- src/Model/Database.php
+++ src/Model/Database.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Model;
 
 use PDO;
+use Exception;
 
 include "../public/Config.php";
 
@@ -19,7 +20,13 @@ class Database
 
         if (self::$bdd == null) {
             $config = new Config;
-            self::$bdd = new PDO('mysql:host=localhost; dbname=' . $config->Database_Name, $config->Database_User, $config->Database_Password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            try {
+                self::$bdd = new PDO('mysql:host=localhost; dbname=' . $config->Database_Name, $config->Database_User, $config->Database_Password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            } catch (Exception $e) {
+                echo "Cannot reach Database";
+                die;
+            }
+            
         }
 
         return self::$bdd;

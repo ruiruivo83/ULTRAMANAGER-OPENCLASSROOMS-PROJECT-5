a736265207c9595358b76c041c16e7ddbe2d15f2
Rui <ruivo.rui@gmail.com>
2020-01-22 17:08:56 +0100

    Update

--- public/Config.php
+++ public/Config.php
@@ -10,22 +10,21 @@ require '../vendor/autoload.php';
 // ROUTER FOR CONFIG PAGE
 class Config
 {
-  
-   // LOCAL DATABASE
-    public $Database_Name = "power";
-    public $Database_User = "root";
-    public $Database_Password = "";
-
+    // REMOTE DATABASE
+    public $Database_Name = "_power";
+    public $Database_User = "p5root";
+    public $Database_Password = "ycvms74b";
+   
 }
 
 /*
 
- 
+ // LOCAL DATABASE
+    public $Database_Name = "power";
+    public $Database_User = "root";
+    public $Database_Password = "";
 
-  // REMOTE DATABASE
-    public $Database_Name = "_power";
-    public $Database_User = "p5root";
-    public $Database_Password = "ycvms74b";
+
 
 
  */

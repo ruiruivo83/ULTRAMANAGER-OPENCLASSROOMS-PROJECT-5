8976f820f73795d8b5b886031c1986eede4a67f5
Rui <ruivo.rui@gmail.com>
2020-04-13 11:33:04 +0200

    Update

---
+++ src/Empty_Config.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tools;
+
+
+// COMPOSER AUTOLOAD
+// require '../vendor/autoload.php';
+
+
+// ROUTER FOR CONFIG PAGE
+class Config
+{
+
+    // LOCAL DATABASE
+    public $Database_Name = "power";
+    public $Database_User = "root";
+    public $Database_Password = "";
+
+
+}
+
+    /*
+
+    // OVH DATABASE
+    public $Database_Name = "ultragesyrp5root";
+    public $Database_User = "ultragesyrp5root";
+    public $Database_Password = "SCxaP3HSsVbC";
+
+    // GoDaddy DATABASE
+    public $Database_Name = "_power";
+    public $Database_User = "p5root";
+    public $Database_Password = "ycvms74b";
+
+    // O2SWITCH
+    public $Database_Name = "ruru4671_ultra";
+    public $Database_User = "ruru4671_p5root";
+    public $Database_Password = "*Ycvms74b";
+
+    */
